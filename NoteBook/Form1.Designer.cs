0f5fbe8005371e7d93161bf3889b5ec58b137676
zeynep ötegen <zeynepotegen2006@gmail.com>
2025-11-16 18:20:11 +0300

    Proje dosyası ekle.

---
+++ NoteBook/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace NoteBook
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        #region Windows Form Designer üretilen kod
+
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.lblBaslik = new System.Windows.Forms.Label();
+            this.txtBaslik = new System.Windows.Forms.TextBox();
+            this.txtNote = new System.Windows.Forms.RichTextBox();
+            this.panelButtons = new System.Windows.Forms.Panel();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnAra = new System.Windows.Forms.Button();
+            this.groupBoxList = new System.Windows.Forms.GroupBox();
+            this.lstNot = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.panelButtons.SuspendLayout();
+            this.groupBoxList.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(900, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+                                      | System.Windows.Forms.AnchorStyles.Left) 
+                                      | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(12, 36);
+            this.splitContainer1.Name = "splitContainer1";
+            // left = editor, right = list
+            this.splitContainer1.Panel1.Padding = new System.Windows.Forms.Padding(8);
+            this.splitContainer1.Panel2.Padding = new System.Windows.Forms.Padding(8);
+            this.splitContainer1.Size = new System.Drawing.Size(876, 612);
+            this.splitContainer1.SplitterDistance = 560;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // lblBaslik
+            // 
+            this.lblBaslik.AutoSize = true;
+            this.lblBaslik.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblBaslik.Location = new System.Drawing.Point(11, 11);
+            this.lblBaslik.Name = "lblBaslik";
+            this.lblBaslik.Size = new System.Drawing.Size(44, 15);
+            this.lblBaslik.TabIndex = 0;
+            this.lblBaslik.Text = "Başlık:";
+            // 
+            // txtBaslik
+            // 
+            this.txtBaslik.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+                                     | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtBaslik.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.txtBaslik.Location = new System.Drawing.Point(14, 30);
+            this.txtBaslik.Name = "txtBaslik";
+            this.txtBaslik.Size = new System.Drawing.Size(524, 25);
+            this.txtBaslik.TabIndex = 1;
+            this.txtBaslik.TextChanged += new System.EventHandler(this.txtBaslik_TextChanged);
+            // 
+            // txtNote
+            // 
+            this.txtNote.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+                                     | System.Windows.Forms.AnchorStyles.Left) 
+                                     | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtNote.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.txtNote.Location = new System.Drawing.Point(14, 65);
+            this.txtNote.Name = "txtNote";
+            this.txtNote.Size = new System.Drawing.Size(524, 472);
+            this.txtNote.TabIndex = 2;
+            this.txtNote.Text = "";
+            this.txtNote.TextChanged += new System.EventHandler(this.txtNote_TextChanged);
+            // 
+            // panelButtons
+            // 
+            this.panelButtons.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.panelButtons.Controls.Add(this.btnKaydet);
+            this.panelButtons.Controls.Add(this.btnEkle);
+            this.panelButtons.Controls.Add(this.btnSil);
+            this.panelButtons.Controls.Add(this.btnAra);
+            this.panelButtons.Location = new System.Drawing.Point(14, 548);
+            this.panelButtons.Name = "panelButtons";
+            this.panelButtons.Size = new System.Drawing.Size(524, 48);
+            this.panelButtons.TabIndex = 3;
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.btnKaydet.Location = new System.Drawing.Point(0, 6);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(96, 36);
+            this.btnKaydet.TabIndex = 0;
+            this.btnKaydet.Text = "Kaydet";
+            this.btnKaydet.UseVisualStyleBackColor = true;
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.btnEkle.Location = new System.Drawing.Point(110, 6);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(96, 36);
+            this.btnEkle.TabIndex = 1;
+            this.btnEkle.Text = "Yeni Not";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnYeni_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.btnSil.Location = new System.Drawing.Point(220, 6);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(96, 36);
+            this.btnSil.TabIndex = 2;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // btnAra
+            // 
+            this.btnAra.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.btnAra.Location = new System.Drawing.Point(330, 6);
+            this.btnAra.Name = "btnAra";
+            this.btnAra.Size = new System.Drawing.Size(96, 36);
+            this.btnAra.TabIndex = 3;
+            this.btnAra.Text = "Ara";
+            this.btnAra.UseVisualStyleBackColor = true;
+            // 
+            // groupBoxList
+            // 
+            this.groupBoxList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+                                         | System.Windows.Forms.AnchorStyles.Left) 
+                                         | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBoxList.Controls.Add(this.lstNot);
+            this.groupBoxList.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.groupBoxList.Location = new System.Drawing.Point(8, 8);
+            this.groupBoxList.Name = "groupBoxList";
+            this.groupBoxList.Size = new System.Drawing.Size(292, 596);
+            this.groupBoxList.TabIndex = 0;
+            this.groupBoxList.TabStop = false;
+            this.groupBoxList.Text = "Notlar";
+            // 
+            // lstNot
+            // 
+            this.lstNot.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lstNot.FormattingEnabled = true;
+            this.lstNot.ItemHeight = 16;
+            this.lstNot.Location = new System.Drawing.Point(3, 19);
+            this.lstNot.Name = "lstNot";
+            this.lstNot.Size = new System.Drawing.Size(286, 574);
+            this.lstNot.TabIndex = 0;
+            this.lstNot.SelectedIndexChanged += new System.EventHandler(this.lstNot_SelectedIndexChanged);
+            // 
+            // Wire controls into splitContainer panels
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.lblBaslik);
+            this.splitContainer1.Panel1.Controls.Add(this.txtBaslik);
+            this.splitContainer1.Panel1.Controls.Add(this.txtNote);
+            this.splitContainer1.Panel1.Controls.Add(this.panelButtons);
+            this.splitContainer1.Panel2.Controls.Add(this.groupBoxList);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(900, 660);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.Name = "Form1";
+            this.Text = "Not Defteri";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.panelButtons.ResumeLayout(false);
+            this.groupBoxList.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Label lblBaslik;
+        private System.Windows.Forms.RichTextBox txtNote;
+        private System.Windows.Forms.TextBox txtBaslik;
+        private System.Windows.Forms.Panel panelButtons;
+        private System.Windows.Forms.Button btnKaydet;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnAra;
+        private System.Windows.Forms.GroupBox groupBoxList;
+        private System.Windows.Forms.ListBox lstNot;
+    }
+}
---
+++ NoteBook/Form1.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace NoteBook
+{
+    public partial class Form1 : Form
+    {
+        private BindingList<Note> notes = new BindingList<Note>();
+        private int currentNoteIndex = -1;
+        private readonly string notesFilePath = Path.Combine(Application.StartupPath, "notes.xml");
+        private bool isNewNote = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class Note
+        {
+            public string Title { get; set; }
+            public string Content { get; set; }
+            public DateTime CreatedDate { get; set; }
+
+            public override string ToString()
+            {
+                return $"{Title} - {CreatedDate:dd.MM.yyyy HH:mm}";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "Not Defteri Uygulaması";
+
+            // ListBox'ı veri kaynağı ile bağla
+            lstNot.DataSource = notes;
+
+            // Notları yükle
+            LoadNotes();
+
+            // Kontrolleri temizle
+            ClearForm();
+
+            UpdateStatus("Uygulama hazır");
+        }
+
+        private void ClearForm()
+        {
+            txtBaslik.Clear();
+            txtNote.Clear();
+            currentNoteIndex = -1;
+            isNewNote = true;
+            lstNot.ClearSelected();
+            btnSil.Enabled = false;
+        }
+
+        private void UpdateNotesList()
+        {
+            // BindingList otomatik güncellenir, manuel güncellemeye gerek yok
+            // Sadece seçimi güncelle - GÜVENLİ VERSİYON
+            if (currentNoteIndex >= 0 && currentNoteIndex < notes.Count && lstNot.Items.Count > currentNoteIndex)
+            {
+                try
+                {
+                    lstNot.SelectedIndex = currentNoteIndex;
+                }
+                catch (ArgumentOutOfRangeException)
+                {
+                    // Eğer index geçersizse, son geçerli index'i seç veya seçimi temizle
+                    if (lstNot.Items.Count > 0)
+                    {
+                        lstNot.SelectedIndex = lstNot.Items.Count - 1;
+                        currentNoteIndex = lstNot.SelectedIndex;
+                    }
+                    else
+                    {
+                        lstNot.ClearSelected();
+                        currentNoteIndex = -1;
+                    }
+                }
+            }
+            else if (notes.Count == 0)
+            {
+                lstNot.ClearSelected();
+                currentNoteIndex = -1;
+            }
+        }
+
+        private void btnYeni_Click(object sender, EventArgs e)
+        {
+            // Mevcut not kaydedilmemişse kaydetmek isteyip istemediğini sor
+            if (!IsFormEmpty() && isNewNote)
+            {
+                DialogResult result = MessageBox.Show(
+                    "Mevcut not kaydedilsin mi?",
+                    "Kaydet",
+                    MessageBoxButtons.YesNoCancel,
+                    MessageBoxIcon.Question);
+
+                if (result == DialogResult.Yes)
+                {
+                    SaveCurrentNote();
+                }
+                else if (result == DialogResult.Cancel)
+                {
+                    return;
+                }
+            }
+
+            ClearForm();
+            txtBaslik.Focus();
+            UpdateStatus("Yeni not oluşturuldu");
+        }
+
+        private bool IsFormEmpty()
+        {
+            return string.IsNullOrWhiteSpace(txtBaslik.Text) &&
+                   string.IsNullOrWhiteSpace(txtNote.Text);
+        }
+
+        private void SaveCurrentNote()
+        {
+            if (string.IsNullOrWhiteSpace(txtBaslik.Text))
+            {
+                MessageBox.Show("Lütfen bir başlık girin.", "Uyarı",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtBaslik.Focus();
+                return;
+            }
+
+            if (isNewNote)
+            {
+                // Yeni not oluştur
+                Note newNote = new Note
+                {
+                    Title = txtBaslik.Text.Trim(),
+                    Content = txtNote.Text,
+                    CreatedDate = DateTime.Now
+                };
+
+                notes.Add(newNote);
+                currentNoteIndex = notes.Count - 1;
+                isNewNote = false;
+                UpdateStatus("Yeni not kaydedildi");
+            }
+            else if (currentNoteIndex >= 0 && currentNoteIndex < notes.Count)
+            {
+                // Mevcut notu güncelle
+                notes[currentNoteIndex].Title = txtBaslik.Text.Trim();
+                notes[currentNoteIndex].Content = txtNote.Text;
+                UpdateStatus("Not güncellendi");
+            }
+
+            SaveNotes();
+            UpdateNotesList(); // Bu metod zaten seçim işlemini yapıyor
+            UpdateDeleteButtonState();
+        }
+
+
+
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            SaveCurrentNote();
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            if (lstNot.SelectedIndex < 0 || currentNoteIndex < 0)
+            {
+                MessageBox.Show("Lütfen silmek için bir not seçin.", "Bilgi",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            var noteToDelete = notes[currentNoteIndex];
+            var result = MessageBox.Show(
+                $"\"{noteToDelete.Title}\" başlıklı not silinsin mi?",
+                "Not Sil",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                notes.RemoveAt(currentNoteIndex);
+                SaveNotes();
+                ClearForm();
+                UpdateStatus("Not silindi");
+
+                // Listenin son öğesi silindiyse, bir öncekini seç
+                if (notes.Count > 0)
+                {
+                    if (currentNoteIndex >= notes.Count)
+                    {
+                        currentNoteIndex = notes.Count - 1;
+                    }
+                    if (currentNoteIndex >= 0 && lstNot.Items.Count > currentNoteIndex)
+                    {
+                        lstNot.SelectedIndex = currentNoteIndex;
+                    }
+                }
+            }
+        }
+
+        private void lstNot_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lstNot.SelectedIndex >= 0 && lstNot.SelectedIndex < notes.Count)
+            {
+                currentNoteIndex = lstNot.SelectedIndex;
+                Note selectedNote = notes[currentNoteIndex];
+
+                txtBaslik.Text = selectedNote.Title;
+                txtNote.Text = selectedNote.Content;
+                isNewNote = false;
+
+                UpdateDeleteButtonState();
+                UpdateStatus($"Not yüklendi: {selectedNote.Title}");
+            }
+            else if (lstNot.SelectedIndex == -1 && notes.Count > 0)
+            {
+                // Seçim kaldırıldıysa ama notlar varsa
+                UpdateDeleteButtonState();
+            }
+        }
+
+        private void UpdateDeleteButtonState()
+        {
+            // ListBox'ta seçili öğe varsa veya geçerli bir not index'i varsa sil butonunu aktif et
+            btnSil.Enabled = (lstNot.SelectedIndex >= 0) ||
+                             (currentNoteIndex >= 0 && currentNoteIndex < notes.Count);
+        }
+
+        private void UpdateStatus(string message)
+        {
+            // StatusStrip varsa:
+            // toolStripStatusLabel1.Text = message;
+
+            // Veya form başlığına ekle:
+            this.Text = $"Not Defteri - {message}";
+        }
+
+        private void SaveNotes()
+        {
+            try
+            {
+                var serializer = new XmlSerializer(typeof(List<Note>));
+                using (var stream = File.Create(notesFilePath))
+                {
+                    serializer.Serialize(stream, notes.ToList());
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Notlar kaydedilemedi: {ex.Message}", "Hata",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void LoadNotes()
+        {
+            try
+            {
+                if (!File.Exists(notesFilePath))
+                    return;
+
+                var serializer = new XmlSerializer(typeof(List<Note>));
+                using (var stream = File.OpenRead(notesFilePath))
+                {
+                    var loadedNotes = (List<Note>)serializer.Deserialize(stream);
+                    if (loadedNotes != null)
+                    {
+                        notes = new BindingList<Note>(loadedNotes);
+
+                        // DATA SOURCE'U YENİDEN ATA - BUNU EKLE
+                        lstNot.DataSource = notes;
+                    }
+                }
+                UpdateStatus($"{notes.Count} not yüklendi");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Notlar yüklenemedi: {ex.Message}", "Hata",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                notes = new BindingList<Note>();
+                // BU DURUMDA DA DATA SOURCE'U GÜNCELLE
+                lstNot.DataSource = notes;
+            }
+            }
+
+        // TextBox değişikliklerinde isNewNote durumunu güncelle
+        private void txtBaslik_TextChanged(object sender, EventArgs e)
+        {
+            if (lstNot.SelectedIndex < 0)
+            {
+                isNewNote = true;
+            }
+        }
+
+        private void txtNote_TextChanged(object sender, EventArgs e)
+        {
+            if (lstNot.SelectedIndex < 0)
+            {
+                isNewNote = true;
+            }
+        }
+    }
+}
